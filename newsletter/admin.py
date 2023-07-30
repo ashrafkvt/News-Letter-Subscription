a76922078ad18e7a7d6886e8e0a5489a94b5bf30
ashrafkvt <ashrafkvt6197@gmail.com>
2023-07-30 07:15:42 +0530

    write celery tasks and setup admin

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from . import tasks
+from .models import Newsletter, Subscriber, Issue, Subscription
+
+admin.site.register(Subscription)
+
+
+def send(modeladmin, request, queryset):
+    for issue in queryset:
+        tasks.send_issue.delay(issue.id)
+
+
+send.short_description = "send"
+
+
+class IssueInline(admin.StackedInline):
+    model = Issue
+
+
+@admin.register(Issue)
+class IssueAdmin(admin.ModelAdmin):
+    actions = [send]
+
+
+class SubscriptionAdmin(admin.StackedInline):
+    model = Subscription
+    pass
+
+
+@admin.register(Newsletter)
+class NewsletterAdmin(admin.ModelAdmin):
+    inlines = (IssueInline, SubscriptionAdmin)
+
+
+@admin.register(Subscriber)
+class SubscriberAdmin(admin.ModelAdmin):
+    inlines = (SubscriptionAdmin,)
